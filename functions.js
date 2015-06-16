70584180a747e2b6e701e5d85378290035d87178
Elliott Moore <mooreorless42@gmail.com>
2015-06-17 06:58:11 +1000

    initial push

---
+++ functions.js
@@ -0,0 +1,22 @@
+ $(function() {
+     smoothScroll(400);
+ });
+
+// Smooth scroll function is applied to the body text
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
+
+                          
+                          
